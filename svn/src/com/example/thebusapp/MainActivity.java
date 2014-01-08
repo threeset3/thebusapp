1ee37ad99a9b5236b72e0ff38c1108dc9755dfb6
threeset3 <threeset3@gmail.com>
2014-01-07 23:09:12 -0500

    MapView integration - testing 2

--- svn/src/com/example/thebusapp/MainActivity.java
+++ svn/src/com/example/thebusapp/MainActivity.java
@@ -2,11 +2,9 @@ package com.example.thebusapp;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -16,13 +14,10 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-/*
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-*/
 
+	public void mapView(View view) {
+		Intent i = new Intent(this, MapView.class);
+		startActivity(i);
+	}
+	
 }
